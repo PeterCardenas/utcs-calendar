66b4d3824990d6f386c17baeaf5d9017b509b5b0
Peter Cardenas <petercardenas441@gmail.com>
2019-09-17 02:41:41 -0500

    modularized write_csv and created search for existing UTCS Google Calendar

--- utcs-calendar/utcs_calendar.py
+++ utcs-calendar/utcs_calendar.py
@@ -11,7 +11,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/calendar']
 
 CHROME_PATH = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
@@ -36,6 +36,99 @@ def combine_description(description_parts):
     return desc
 
 
+def get_address():
+    location_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-location div.field-items")
+    location_name = ""
+    if len(location_elements) > 0:
+        location_name = location_elements[0].text
+    else:
+        room_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-room div.field-items")
+        if len(room_elements) > 0:
+            location_name = room_elements[0].text
+        else:
+            location_name = "GDC"
+
+    location = ""
+    if "GDC" in location_name:
+        location = "Gates Dell Complex"
+    elif "POB" in location_name:
+        location = "Peter O'Donnell Jr Building"
+    elif "1.304" in location_name:
+        location = "Gates Dell Complex"
+    elif "EER" in location_name:
+        location = "Engineering Education and Research Center (EER)"
+    elif "AT&T" in location_name:
+        location = "AT&T Executive Education and Conference Center"
+    elif "Union" in location_name:
+        location = "Texas Union"
+    elif "Etter-Harbin" in location_name:
+        location = "Etter-Harbin Alumni Center"
+    elif "Frank Erwin" in location_name:
+        location = "Frank C. Erwin, Jr., Special Events Center"
+    elif "Pizza" in location_name:
+        location = "The Varsity Pizza & Pints"
+    elif "Palmer" in location_name:
+        location = "Palmer Events Center"
+
+    if len(location) == 0:
+        print("No address stored for location name:", location_name)
+
+    return location
+
+
+def get_time():
+    start_time_elements = driver.find_elements(By.CSS_SELECTOR, "span.date-display-start")
+    start_time = ""
+    end_time_elements = driver.find_elements(By.CSS_SELECTOR, "span.date-display-end")
+    end_time = ""
+    all_day = "False"
+    if len(start_time_elements) > 0:
+        start_time = start_time_elements[0].text
+        start_time = start_time[:-2] + " " + start_time[-2:].upper()
+        if len(end_time_elements) > 0:
+            end_time = end_time_elements[0].text
+            end_time = end_time[:-2] + " " + end_time[-2:].upper()
+    else:
+        all_day = "True"
+
+    ret = {
+        start_time: start_time,
+        end_time: end_time,
+        all_day: all_day
+    }
+    return ret
+
+
+def get_contact():
+    contact_name_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-name div.field-items")
+    contact_name = ""
+    if len(contact_name_elements) > 0:
+        contact_name = contact_name_elements[0].text
+
+    contact_email_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-email div.field-items")
+    contact_email = ""
+    if len(contact_email_elements) > 0:
+        contact_email = contact_email_elements[0].text
+
+    return contact_name, contact_email
+
+
+def get_description(contact_name, contact_email, url):
+    description_parts = driver.find_elements(By.CSS_SELECTOR, "div.field-type-text-with-summary p")
+    description = ""
+    if len(description_parts) > 0:
+        description = combine_description(description_parts)
+
+    description = "\"{0}{1}URL: {2}\n\n{3}\"".format(
+        "Contact Name: " + contact_name + "\n\n" if len(contact_name) > 0 else contact_name,
+        "Contact Email: " + contact_email + "\n\n" if len(contact_email) > 0 else contact_email,
+        url,
+        description
+    )
+
+    return description
+
+
 def write_csv():
     csv = open("events.csv", "w+")
     year = 2019
@@ -53,86 +146,25 @@ def write_csv():
             url = driver.current_url
             subject = driver.find_element(By.CSS_SELECTOR, "h1#page-title").text
             date = driver.find_element(By.CSS_SELECTOR, "span.date-display-single").text.split(" ")[1]
-            all_day = "False"
 
-            start_time_elements = driver.find_elements(By.CSS_SELECTOR, "span.date-display-start")
-            start_time = ""
-            if len(start_time_elements) > 0:
-                start_time = start_time_elements[0].text
-                start_time = start_time[:-2] + " " + start_time[-2:].upper()
-                end_time_elements = driver.find_elements(By.CSS_SELECTOR, "span.date-display-end")
-                end_time = ""
-                if len(end_time_elements) > 0:
-                    end_time = end_time_elements[0].text
-                    end_time = end_time[:-2] + " " + end_time[-2:].upper()
-            else:
-                all_day = "True"
+            time = get_time()
 
-            description_parts = driver.find_elements(By.CSS_SELECTOR, "div.field-type-text-with-summary p")
-            description = ""
-            if len(description_parts) > 0:
-                description = combine_description(description_parts)
+            contact_name, contact_email = get_contact()
 
-            contact_name_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-name div.field-items")
-            contact_name = ""
-            if len(contact_name_elements) > 0:
-                contact_name = contact_name_elements[0].text
+            location = get_address()
 
-            contact_email_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-email div.field-items")
-            contact_email = ""
-            if len(contact_email_elements) > 0:
-                contact_email = contact_email_elements[0].text
-
-            location_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-location div.field-items")
-            location = ""
-            if len(location_elements) > 0:
-                location = location_elements[0].text
-            else:
-                room_elements = driver.find_elements(By.CSS_SELECTOR, "div.field-name-field-room div.field-items")
-                if len(room_elements) > 0:
-                    location = room_elements[0].text
-                else:
-                    location = "GDC"
-            if "GDC" in location:
-                location = "Gates Dell Complex"
-            elif "POB" in location:
-                location = "Peter O'Donnell Jr Building"
-            elif "1.304" in location:
-                location = "Gates Dell Complex"
-            elif "EER" in location:
-                location = "Engineering Education and Research Center (EER)"
-            elif "AT&T" in location:
-                location = "AT&T Executive Education and Conference Center"
-            elif "Union" in location:
-                location = "Texas Union"
-            elif "Etter-Harbin" in location:
-                location = "Etter-Harbin Alumni Center"
-            elif "Frank Erwin" in location:
-                location = "Frank C. Erwin, Jr., Special Events Center"
-            elif "Pizza" in location:
-                location = "The Varsity Pizza & Pints"
-            elif "Palmer" in location:
-                location = "Palmer Events Center"
-            else:
-                print("Subject:", subject, "Date:", date, "Location:", location, "URL:", url)
-
-            description = "\"{0}{1}URL: {2}\n\n{3}\"".format(
-                "Contact Name: " + contact_name + "\n\n" if len(contact_name) > 0 else contact_name,
-                "Contact Email: " + contact_email + "\n\n" if len(contact_email) > 0 else contact_email,
-                url,
-                description
-            )
+            description = get_description(contact_name, contact_email, url)
             location = "\"" + location + "\""
             subject = "\"" + subject + "\""
             csv.write(subject + "," +
                       date + "," +
-                      start_time + "," +
+                      time.get("start_time") + "," +
                       date + "," +
-                      end_time + "," +
-                      all_day + "," +
+                      time.get("end_time") + "," +
+                      time.get("all_day") + "," +
                       description + "," +
                       location + "," +
-                      "False"
+                      "False" +
                       "\n")
             driver.get(month_link)
             events = driver.find_elements(By.CSS_SELECTOR, "td.single-day div.contents")
@@ -174,17 +206,18 @@ def google_calendar():
 def upload_events(calendar):
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = calendar.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=10, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print('No upcoming events found.')
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+    page_token = None
+    next_page_exists = True
+    calendar_list = calendar.calendarList().list(pageToken=page_token).execute()
+    calendar = None
+    while not calendar and next_page_exists:
+        for calendar_list_entry in calendar_list['items']:
+            if calendar_list_entry['summary'] == 'UTCS':
+                calendar = calendar_list_entry
+        if not calendar:
+            page_token = calendar_list.get('nextPageToken')
+            if not page_token:
+                next_page_exists = False
 
 
 def main():
